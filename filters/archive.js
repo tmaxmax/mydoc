b4c0abc8b6db47b451d597226fba483c5b82183e
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2026-05-23 02:00:36 +0200

    Implement archive using JavaScript pandoc filter

---
+++ filters/archive.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+import katex from "katex";
+import pandoc from "pandoc-filter";
+import hljs from "highlight.js";
+
+pandoc.stdio((value, format, meta) => {
+  if (value.t === "Math") {
+    console.error(value.c);
+    const [type, math] = value.c;
+    const html = katex.renderToString(math.trim(), {
+      displayMode: type.t === "DisplayMath",
+      output: "htmlAndMathml",
+      throwOnError: false,
+    });
+
+    return pandoc.RawInline("html", html);
+  }
+
+  if (value.t === "CodeBlock") {
+    const [[, [language] = []], code] = value.c;
+    const html = `<pre><code class="hljs hljs-${language}">${hljs.highlight(code, { language }).value}</code></pre>`;
+
+    return pandoc.RawBlock("html", html);
+  }
+
+  if (value.t === "Code") {
+    const [, code] = value.c;
+    const html = `<code class="hljs">${hljs.highlight(code, { language: "plaintext" }).value}</code>`;
+
+    return pandoc.RawInline("html", html);
+  }
+});
