cfcd4311210327968d4a436c8f3181331c30dba7
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2026-05-30 17:53:34 +0200

    Handle hyphenation and poem indentation

---
+++ filters/hyphens.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+import pandoc from "pandoc-filter";
+
+pandoc.stdio((value, format, meta) => {
+  const c = getInlineContent(value);
+  for (let i = 0; i < c.length; i++) {
+    const m = c[i];
+    if (m.t === "Str") {
+      m.c = m.c.replace(/(\w)-(\w)/g, "$1\u{2011}$2");
+    }
+  }
+  return value;
+});
+
+/**
+ * @param {pandoc.AnyElt} value
+ * @return {pandoc.Inline[]}
+ */
+function getInlineContent(value) {
+  switch (value.t) {
+    case "Para":
+      return value.c;
+    case "Header":
+      return value.c[2];
+    case "Plain":
+      return value.c;
+    default:
+      return [];
+  }
+}
