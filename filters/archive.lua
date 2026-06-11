e7c9d5c26f36e226a5c7a2d839e660b665c1d032
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2026-06-11 05:06:48 +0200

    Format Lua

--- filters/archive.lua
+++ filters/archive.lua
@@ -2,7 +2,7 @@ function Pandoc(doc)
   local has_math = false
 
   pcall(function()
-    doc:walk{
+    doc:walk {
       Math = function()
         has_math = true
         error({}, 0)
@@ -12,9 +12,9 @@ function Pandoc(doc)
 
   doc.meta.math = pandoc.MetaBool(has_math)
   if has_math and doc.meta["dev.skip_fonts"] == nil then
-    pandoc.pipe("python3", {"patch_katex_fonts.py"}, "")
+    pandoc.pipe("python3", { "patch_katex_fonts.py" }, "")
   end
 
-  local out = pandoc.pipe("node", {"--experimental-strip-types", "filters/archive.ts"}, pandoc.write(doc, "json"))
+  local out = pandoc.pipe("node", { "--experimental-strip-types", "filters/archive.ts" }, pandoc.write(doc, "json"))
   return pandoc.read(out, "json")
 end
