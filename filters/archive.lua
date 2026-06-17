64b7061b1a0ced6da5d4455cd5a458a10e5fc093
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2026-06-17 22:56:31 +0300

    fix

--- filters/archive.lua
+++ filters/archive.lua
@@ -1,5 +1,5 @@
 function Pandoc(doc)
-  pandoc.log.info(json.encode(doc.meta.Tree))
+  pandoc.log.info(pandoc.json.encode(doc.meta.Tree))
   local out = pandoc.pipe("node", { "--experimental-strip-types", "filters/archive.ts" }, pandoc.write(doc, "json"))
   return pandoc.read(out, "json")
 end
