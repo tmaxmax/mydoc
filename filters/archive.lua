73260606ed460576c7d3bca355f4f5b645645e3f
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2026-06-17 22:54:44 +0300

    Log index tree in pandoc filter

--- filters/archive.lua
+++ filters/archive.lua
@@ -1,4 +1,5 @@
 function Pandoc(doc)
+  pandoc.log.info(json.encode(doc.meta.Tree))
   local out = pandoc.pipe("node", { "--experimental-strip-types", "filters/archive.ts" }, pandoc.write(doc, "json"))
   return pandoc.read(out, "json")
 end
